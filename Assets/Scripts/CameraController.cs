ebec1273aeeb8429fde2095d1973946782d9b097
Ardjen Lane <ardjenlane@hotmail.com>
2017-03-21 13:57:08 -0400

    project setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        camera.transform.Translate(move);
+	}
+}
---
+++ Assets/Scripts/RenderController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderController : MonoBehaviour {
+
+    private Sprite[] sdvSpring;
+
+    void Awake()
+    {
+        //Load Sprite Sheets
+        sdvSpring = Resources.LoadAll<Sprite>("SpriteSheets/sdv_spring");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        drawGrass();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void drawGrass()
+    {
+        int x = 100;
+        int y = 100;
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                GameObject obj = new GameObject("Grass");
+                SpriteRenderer ren = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+                ren.sprite = sdvSpring[156];
+                obj.transform.Translate(new Vector2(i, j));
+            }
+        }
+    }
+}
